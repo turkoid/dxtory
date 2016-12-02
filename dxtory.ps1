497d357c9f3f3c8ebd4dcc31380434665cadd6e8
John Ross <jlross42@gmail.com>
2016-12-02 08:47:48 -0600

    updated to allow static and wildcard filenames

--- dxtory.ps1
+++ dxtory.ps1
@@ -1,26 +1,71 @@
 $dxtoryIgnoreList = "C:\Program Files (x86)\ExKode\Dxtory2.0\ignore_module_list.txt"
 $ignoreList = new-object System.Collections.ArrayList
 
-$props = @{
-    path = "C:\Windows\SysWOW64\Macromed\Flash\"
-    regex = "FlashPlayerPlugin.*\.exe"
-    match = ""
-    found = $false
+function AddWildcardFilename($path, $filename) {
+    $props = @{
+        static = $path -eq ""
+        path = $path
+        regex = $filename
+        match = ""
+        found = $false
+    }
+    if ($props.static) {
+        $props.match = $filename
+        $props.regex = $props.regex -replace "[[+*?()\\.]", "\$&"
+    }
+    
+    $ignore = new-object psobject -Property $props
+    [void] $ignoreList.Add($ignore)
 }
-$ignore = new-object psobject -Property $props
-[void] $ignoreList.Add($ignore)
 
+function AddStaticFilename($filename) {
+    AddWildcardFilename "" $filename
+}
+
+#wildcard filenames
+AddWildcardFilename "C:\Windows\SysWOW64\Macromed\Flash\" "FlashPlayerPlugin.*\.exe"
+
+#static filenames
+
+AddStaticFilename "Discord.exe"
+AddStaticFilename "DiscordPTB.exe"
+AddStaticFilename "slack.exe"
+AddStaticFilename "Google Play Music Desktop Player.exe"
+AddStaticFilename "Google%20Play%20Music%20Desktop%20Player.exe"
+AddStaticFilename "zune.exe"
+AddStaticFilename "BodySlide x64.exe"
+AddStaticFilename "ReShade Assistant Preview.exe"
+AddStaticFilename "TeamViewer.exe"
+AddStaticFilename "Battle.net.exe"
+AddStaticFilename "HD-Frontend.exe"
 foreach ($ignore in $ignoreList) {
-    $ignore.regex = "^" + $ignore.regex
-    $ignoreMatch = gci $ignore.path | ? {$_.name -match $ignore.regex} | select -first 1
-    $ignore.match = $ignoreMatch.name
+    if (!($ignore.static)) {
+        $regex = "^" + $ignore.regex
+        $ignoreMatch = gci $ignore.path | ? {$_.name -match $regex} | select -first 1
+        $ignore.match = $ignoreMatch.name
+    } 
+    if ($ignore.match -ne "") {
+        $ignore.match = "*\" + $ignore.match
+    }
 }
 
 (gc $dxtoryIgnoreList) | `
-    foreach { foreach ($ignore in $ignoreList) { 
-        $ignore.found = $ignore.found -or ($_ -match $ignore.regex)
-        $_ -replace $ignore.regex, $ignore.match
-    }} | `
+    foreach {        
+        $replace = ""
+        foreach ($ignore in $ignoreList) { 
+            $regex = "^\*?\\?" + $ignore.regex
+            $match = ($_ -match $regex)
+            $ignore.found = $ignore.found -or $match
+            if ($match) {
+                $replace = $ignore.match
+                break
+            }        
+        }    
+        if ($replace -eq "") {
+            $replace = $_
+        }
+        $_ -replace ".+", $replace
+    } | `
     sc ($dxtoryIgnoreList)
 
 foreach ($ignore in $ignoreList) {
